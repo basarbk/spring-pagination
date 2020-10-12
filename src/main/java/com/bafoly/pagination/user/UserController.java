877c4bdb07ac0dc6a06124293849a5d66502249f
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-12 20:23:09 +0300

    get all users

--- src/main/java/com/bafoly/pagination/user/UserController.java
+++ src/main/java/com/bafoly/pagination/user/UserController.java
@@ -1,5 +1,20 @@
 package com.bafoly.pagination.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
-  
+
+  @Autowired
+  UserService userService;
+
+  @GetMapping("/api/1.0/users")
+  List<User> getUsers(){
+    return userService.getUsers();
+  }
+
 }
--- src/main/java/com/bafoly/pagination/user/UserService.java
+++ src/main/java/com/bafoly/pagination/user/UserService.java
@@ -1,5 +1,18 @@
 package com.bafoly.pagination.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+  @Autowired
+  UserRepository userRepository;
+  
+	public List<User> getUsers() {
+		return userRepository.findAll();
+	}
   
 }
