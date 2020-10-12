87ccfab526230625b21ffcf66fe14b0227a3ff49
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-12 20:55:50 +0300

    pageable params

--- src/main/java/com/bafoly/pagination/user/UserController.java
+++ src/main/java/com/bafoly/pagination/user/UserController.java
@@ -3,6 +3,8 @@ package com.bafoly.pagination.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +19,8 @@ public class UserController {
     return userService.getUsers(page);
   }
 
+  @GetMapping("/api/1.0/ordered-users")
+  Page<User> getUsersAlternative(@PageableDefault(sort="id", direction = Direction.DESC, size = 10) Pageable page) {
+    return userService.getUsers(page);
+  }
 }
