22e8223458ad24fca4b867d8c4f8b1fcecc016af
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-12 20:46:14 +0300

    using pageable

--- src/main/java/com/bafoly/pagination/user/UserController.java
+++ src/main/java/com/bafoly/pagination/user/UserController.java
@@ -2,8 +2,8 @@ package com.bafoly.pagination.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +13,8 @@ public class UserController {
   UserService userService;
 
   @GetMapping("/api/1.0/users")
-  Page<User> getUsers(@RequestParam int pageSize, @RequestParam(defaultValue = "0", required = false) int pageNumber) {
-    return userService.getUsers(pageNumber, pageSize);
+  Page<User> getUsers(Pageable page) {
+    return userService.getUsers(page);
   }
 
 }
--- src/main/java/com/bafoly/pagination/user/UserService.java
+++ src/main/java/com/bafoly/pagination/user/UserService.java
@@ -2,7 +2,6 @@ package com.bafoly.pagination.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +10,9 @@ public class UserService {
 
   @Autowired
   UserRepository userRepository;
-  
-	public Page<User> getUsers(int pageNumber, int pageSize) {
-    Pageable pageable = PageRequest.of(pageNumber, pageSize);
-		return userRepository.findAll(pageable);
+
+	public Page<User> getUsers(Pageable page) {
+		return userRepository.findAll(page);
 	}
   
 }
